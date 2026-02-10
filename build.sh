7c18e47de97cf770c6490c422759215af03eef28
unknown <nobelleon.86@gmail.com>
2026-02-10 18:09:53 +0700

    adding vercel

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 # build.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+#!/bin/bash
+
+echo "Building project packages..."
+echo "Installing project requirements..."
+python -m pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
 
