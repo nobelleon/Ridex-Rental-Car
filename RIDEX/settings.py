e4403b95a636d66082057b90f1c40056bdeabfbe
unknown <nobelleon.86@gmail.com>
2026-02-10 17:39:01 +0700

    adding vercel

--- RIDEX/settings.py
+++ RIDEX/settings.py
@@ -116,10 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+#STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+#STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "images/" 
 MEDIA_ROOT = BASE_DIR/'media'
